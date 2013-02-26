963085351ef42e814a94e746411f0a57bd0677b2
sammyrulez <sammyrulez@gmail.com>
2013-02-26 17:00:53 +0100

    init

---
+++ blinkserver/core/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from blinkserver.core.models import BlinkStatus ,Hook
+
+class BlinkStatusInline(admin.TabularInline):
+    model = BlinkStatus
+
+class HookAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    list_fileds=('name',)
+    ordering = ('name',)
+    search_fields = ['name']
+    inlines = [
+        BlinkStatusInline,
+    ]
+
+    def queryset(self, request):
+        qs = super(HookAdmin, self).queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
+
+
+admin.site.register(Hook, HookAdmin)
---
+++ blinkserver/core/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Hook(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
+
+class BlinkStatus(models.Model):
+    red = models.IntegerField()
+    green = models.IntegerField()
+    blue = models.IntegerField()
+    hook =models.ForeignKey(Hook)
+    fading_time = models.FloatField(blank=True, null=True)
+    position = models.PositiveSmallIntegerField("Position")
+    def __str__(self):
+        return "Blink Status %d for %s" %(self.position,self.hook.name)
+    class Meta:
+        ordering = ('position', )
+        verbose_name_plural = "Blink Statuses"
