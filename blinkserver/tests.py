4b7bdb2e2281601aaf2d8c8c455400957728dfa6
sammyrulez <sammyrulez@gmail.com>
2013-02-27 11:09:25 +0100

    added test and fixtures

--- blinkserver/tests.py
+++ blinkserver/tests.py
@@ -8,9 +8,14 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class HookViewTest(TestCase):
+
+    fixtures = ['users', 'hooks']
+
+    def test_public_hook(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+    def test_private_hook(self):
